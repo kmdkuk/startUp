2710fde2206c3bb044b826df18b685c4f0efae6d
kouki <kouworld0123@gmail.com>
2018-08-16 18:12:08 +0900

    typescript用にファイル編集

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,7 +8,7 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
+interface Props { };
 export default class App extends Component<Props> {
   render() {
     return (
